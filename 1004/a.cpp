b9243df07e341a9457c0c8613a90223d6124967a
xueht0525 <867997466@qq.com>
2020-01-29 11:34:01 +0800

    add 1004

---
+++ 1004/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+#ifdef XHT
+	freopen("1.in", "r", stdin);
+//	freopen("2.in", "r", stdin);
+#endif	
+	
+	ios::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+	
+	
+	
+	return 0;	
+}
